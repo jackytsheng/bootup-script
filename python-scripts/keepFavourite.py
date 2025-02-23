cef5a3fa8e81f7b464d6abd580de22896d4b7250
jackytsheng <jackytsheng@gmail.com>
2025-02-23 14:32:38 +1100

    edit python script for non-direct delete case

--- python-scripts/keepFavourite.py
+++ python-scripts/keepFavourite.py
@@ -3,21 +3,24 @@
 import os
 import shutil
 from os import listdir
+from datetime import datetime
 from os.path import isfile, join
 
-favourite = input('folder for favourite file : [Enter if current directory is used] ')
-toDelete = input('folder for file to be deleted : [Enter if current directory is used] ') 
+favourite = input(
+    'folder for favourite file : [Enter if current directory is used] ')
+toDelete = input(
+    'folder for file to be deleted : [Enter if current directory is used] ')
 if favourite == "":
-	print('current directory is used')
-	favourite = "."
+    print('current directory is used')
+    favourite = "."
 else:
-	print('keeping files in directory ' + favourite)
+    print('keeping files in directory ' + favourite)
 
 if toDelete == "":
-	print('current directory is used')
-	toDelete= "."
+    print('current directory is used')
+    toDelete = "."
 else:
-	print('deleting files in directory ' + toDelete)
+    print('deleting files in directory ' + toDelete)
 
 keepfiles = [f for f in listdir(favourite) if isfile(join(favourite, f))]
 namesToKeep = [f.split(".")[0] for f in keepfiles if f.split(".")[0] != ""]
@@ -29,8 +32,8 @@ deletefiles = [f for f in listdir(toDelete) if isfile(join(toDelete, f))]
 
 deleteDict = {}
 for f in deletefiles:
-	if f.split(".")[0] != "":
-		deleteDict[f.split(".")[0]] =  f.split(".")[1]
+    if f.split(".")[0] != "":
+        deleteDict[f.split(".")[0]] = f.split(".")[1]
 
 
 namesToDelete = deleteDict.keys()
@@ -38,7 +41,8 @@ namesToDeleteSet = set(namesToDelete)
 print("files number found to be check for delete is " + str(len(namesToDelete)))
 
 filesToBeDeleted = namesToDeleteSet.difference(namesToKeepSet)
-print("files be deleted {0} keeping {1} ".format(len(filesToBeDeleted),len(namesToDeleteSet) -len(filesToBeDeleted)))
+print("files be deleted {0} keeping {1} ".format(
+    len(filesToBeDeleted), len(namesToDeleteSet) - len(filesToBeDeleted)))
 
 filesToBeDeletedName = [f+"."+deleteDict[f] for f in filesToBeDeleted]
 filesToBeDeletedName.sort()
@@ -47,16 +51,17 @@ print(filesToBeDeletedName)
 confirm = input("do you want to direct delete the files [Y/N] ? ")
 
 if confirm == "Y" or confirm == "y":
-	print("removing files ...")
-	for f in filesToBeDeletedName:
-		os.remove(toDelete+"/"+f)
+    print("removing files ...")
+    for f in filesToBeDeletedName:
+        os.remove(toDelete+"/"+f)
 
-	print("finish removing all files ...")
+    print("finish removing all files ...")
 
 print("moving files into temp folder...")
 
-os.mkdir(toDelete+'/temp')
+tmp_destination = toDelete+'/tmp'+datetime.now().strftime("%Y-%m-%d")
+os.mkdir(tmp_destination)
 for f in filesToBeDeletedName:
-  shutil.copyfile(toDelete+"/"+f,toDelete+'/temp/'+f)
+    shutil.move(toDelete+"/"+f, tmp_destination)
 
 print("finish removing all files ...")
